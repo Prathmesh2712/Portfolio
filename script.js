80e8be68d270528835b89ca836d9382fb3fa39a5
Prathmesh Avinash Chougule <113436941+Prathmesh2712@users.noreply.github.com>
2023-07-05 22:26:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".typing",{
-    strings:["","web-developer","competative programmer"],
+    strings:["","Web-Developer","Competative programmer"],
     typespeed:100,
     Backspeed:60,
     loop:true
